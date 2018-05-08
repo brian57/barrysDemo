e73dcea4c7fcc78969ccae61630962fcb9dea411
Brian Stewart <brian@causecast.org>
2018-05-08 10:23:19 -0700

    Initial Commit

---
+++ BarrysBootcamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarrysBootcamp
+//
+//  Created by Brian Stewart on 5/8/18.
+//  Copyright © 2018 Brian Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
